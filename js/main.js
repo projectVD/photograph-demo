96bf91dcc008b1e934991f343b4c20c98896cb9a
projectVD <vladdegtarov1998@gmail.com>
2020-03-31 16:09:23 +0300

    beta v2, cards animations version 1 is ready

--- js/main.js
+++ js/main.js
@@ -15,4 +15,24 @@ navLinks.forEach(link => {
   link.addEventListener('mouseleave', () => {
     mouseCursor.style.transform = ''
   });
-})
+});
+
+
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+  card.addEventListener('mousemove', startRotate);
+  card.addEventListener('mouseout', stopRotate);
+});
+
+function startRotate(event) {
+  const cardItem = this.querySelector('.card-item');
+  const halfHight = cardItem.offsetHeight / 2;
+  const halfWidth = cardItem.offsetWidth / 2;
+  cardItem.style.transform = 'rotateX('+ -(event.offsetY - halfHight) / 10 +'deg) rotateY('+ (event.offsetX - halfWidth) / 10 +'deg)';
+}
+
+function stopRotate(event) {
+  const cardItem = this.querySelector('.card-item');
+  cardItem.style.transform = 'rotate(0)';
+}
